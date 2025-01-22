e0c21be2564c727169c7f2695596b3de3a33261c
Ivans Mjatežs <imjatezs@edu.riga.lv>
2025-01-22 12:22:41 +0200

    add branch

--- GitNetUzdevums/Form1.Designer.cs
+++ GitNetUzdevums/Form1.Designer.cs
@@ -32,8 +32,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.GroupBoxIMjatezs = new System.Windows.Forms.GroupBox();
+            this.ButtonIMjatezs = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.GroupBoxIMjatezs.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -75,17 +78,39 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Andrejs Liepiņš";
             // 
+            // GroupBoxIMjatezs
+            // 
+            this.GroupBoxIMjatezs.Controls.Add(this.ButtonIMjatezs);
+            this.GroupBoxIMjatezs.Location = new System.Drawing.Point(174, 146);
+            this.GroupBoxIMjatezs.Name = "GroupBoxIMjatezs";
+            this.GroupBoxIMjatezs.Size = new System.Drawing.Size(253, 181);
+            this.GroupBoxIMjatezs.TabIndex = 4;
+            this.GroupBoxIMjatezs.TabStop = false;
+            this.GroupBoxIMjatezs.Text = "IvanMjatezs";
+            // 
+            // ButtonIMjatezs
+            // 
+            this.ButtonIMjatezs.Location = new System.Drawing.Point(70, 65);
+            this.ButtonIMjatezs.Name = "ButtonIMjatezs";
+            this.ButtonIMjatezs.Size = new System.Drawing.Size(115, 49);
+            this.ButtonIMjatezs.TabIndex = 0;
+            this.ButtonIMjatezs.Text = "y=-x^2";
+            this.ButtonIMjatezs.UseVisualStyleBackColor = true;
+            this.ButtonIMjatezs.Click += new System.EventHandler(this.ButtonIMjatezs_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1016, 415);
+            this.Controls.Add(this.GroupBoxIMjatezs);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Funkciju grafiki";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
+            this.GroupBoxIMjatezs.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -96,6 +121,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox GroupBoxIMjatezs;
+        private System.Windows.Forms.Button ButtonIMjatezs;
     }
 }
 
